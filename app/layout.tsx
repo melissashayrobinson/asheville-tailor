f76252601e063d4a73cf3df42cc5af96ee5d5698
Melissa Robinson <melissa.shay.robinson@gmail.com>
2026-06-23 17:40:11 -0400

    smooth scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`${cormorant.variable} ${inter.variable}`}>{children}</body>
+    <html lang="en" className="scroll-smooth">
+        <body className={`${cormorant.variable} ${inter.variable}`}>{children}</body>
     </html>
   );
 }
