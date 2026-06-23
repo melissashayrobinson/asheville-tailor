0b9b386251c1747379db386d5e93318fafa322d2
Melissa Robinson <melissa.shay.robinson@gmail.com>
2026-06-23 19:13:26 -0400

    site map

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://ashevilletailor.com",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://ashevilletailor.com/wedding-dress-alterations-asheville",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+  ];
+}
